1bb65fe863a7ffecfc58080c8e4e4c172180392b
Salman <salmaanmohamed700@gmail.com>
2024-11-25 20:49:10 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+// I have 2 numbers, num 1 and 2 , which you have to multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 x 2 = 64
+
+// I have to generate a random integer greater than 0
+const random = Math.floor(Math.random() * 50) + 10; // math.random gives a number with decimal but math.floor rounds it off
+
+// `mod` test according to indexTest
+const num3 = 34;
+const num4 = 10;
+const mod = num3 % num4; // Remainder is 4
+
+// Find the max number in a set
+const max = Math.max(7, 14, 18, 20); // 20 is the max
+
+
+
+
+
+
+
+
